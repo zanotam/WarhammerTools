ba940cfb41b18594a18d27dc519ee150ee659476
Robert Baker <zanotam@gmail.com>
2019-06-21 12:58:51 -0700

    Doing very basic website creation.

--- WebServer/WebServer.go
+++ WebServer/WebServer.go
@@ -3,13 +3,17 @@ package WebServer Runs the main website back-end for WarhammerTools.com
 */
 package WebServer
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 //LoadWebServer() Starts up the main web server for WarhammerTools.com
 func LoadWebServer() (err error) {
 	//	http.HandleFunc("/test", Responder)
 	fs := http.FileServer(http.Dir("Website/"))
 	http.Handle("/", http.StripPrefix("/", fs))
+	fmt.Printf("launching web server")
 	err = http.ListenAndServe(":80", nil)
 	return
 }
